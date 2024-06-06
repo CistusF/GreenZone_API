77c87d68993b6da2b78618def57e96c435072708
Vendetta <ananddechannel@gmail.com>
2024-06-07 02:47:00 +0900

    - added color, timeline to log

--- src/utils/etc.ts
+++ src/utils/etc.ts
@@ -1,12 +1,28 @@
+import moment from "moment-timezone";
 import { logType } from "../interfaces/interfaces";
-import {} from 'chalk';
+import chalk from 'chalk';
 
-export const logger = (log: string | string[], title?: string, type?: logType): void => {
+export const logger = (log: string | string[], title?: string, typeCode?: logType): void => {
     if (typeof log !== 'string') {
         log.forEach(i => {
-            logger(i, title, type);
+            logger(i, title, typeCode);
         })
     } else {
-        console.log("[%s] : %s", title ?? "GreenZone", log);
+        var logColor = "white";
+        const ctx = new chalk.Instance({ level: 3 });
+        var time = moment().tz("Asia/Seoul").format("HH:mm:ss");
+
+        switch (typeCode) {
+            case -1: // error
+                logColor = "red";
+                break;
+            case 0: // warning
+                logColor = "yellow";
+                break;
+            case 1: // success
+                logColor = "green";
+                break;
+        };
+        console.log(ctx`{bold.${logColor} ✪} [%s / %s] : %s`, "TEST" ?? "GreenZone", time, "test message");
     }
 };
