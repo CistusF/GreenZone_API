ae59e6e3b15c0c4333159469faf5bf1236e2c3dc
Vendetta <ananddechannel@gmail.com>
2024-06-08 16:32:15 +0900

    added null check

--- src/Events/Room/create.ts
+++ src/Events/Room/create.ts
@@ -17,7 +17,7 @@ function isRoomInfoTypes(obj: string | joinUserInfoType | roomInfoType): obj is 
 const createRoom: roomEventObject = {
     eventName: "create",
     run: ({ socket, rooms, members }, room_info) => {
-        if (!isRoomInfoTypes(room_info)) return logger("Cannot found room information", "ROOM CREATOR", -1);
+        if (!room_info || !isRoomInfoTypes(room_info)) return logger("Cannot found room information", "ROOM CREATOR", -1);
         const roomId = createRoomNumber(socket, rooms);
         rooms.push({
             room_number: roomId,
